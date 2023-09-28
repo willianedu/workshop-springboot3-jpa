2b0cfbb01308987b90bb5756640e4fc6c2003715
willianedu <willianeduardo9877@hotmail.com>
2023-09-28 13:08:04 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/repositories/CategoryRepository.java
+++ src/main/java/com/educandoweb/course/repositories/CategoryRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.educandoweb.course.entities.Category;
-import com.educandoweb.course.entities.User;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long>{
