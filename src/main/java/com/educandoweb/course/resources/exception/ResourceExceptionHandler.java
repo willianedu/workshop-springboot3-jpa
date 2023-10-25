91d1852433cd31019638ee47de55dc627c6e76e0
willianedu <willianeduardo9877@hotmail.com>
2023-10-25 13:04:47 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exception/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.educandoweb.course.resources.exception;
 
 import java.time.Instant;
 
+import com.educandoweb.course.services.exception.DatabaseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest  request) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest  request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -3,7 +3,10 @@ package com.educandoweb.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.educandoweb.course.services.exception.DatabaseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
@@ -31,7 +34,13 @@ public class UserService {
     }
     
 	public void delete(Long id) {
-		repository.deleteById(id);	
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e ) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
